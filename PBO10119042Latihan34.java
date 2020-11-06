d75ed38bb00fd9b3d028e5ebbec224c81970abc4
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-11-06 20:24:01 +0700

    Add files via upload

---
+++ PBO10119042Latihan34.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+import java.util.Scanner;
+/**
+ *
+ * @author Muhammad RIfqu Abdillah
+ *  Nama : Muhammad Rifqu Abdillah
+ *  Kelas: Pbo-IF1
+ *  Nim : 10119042 
+ */
+public class PBO10119042Latihan34 {
+    
+    public static void main(String[] args){
+       
+       Scanner scanner = new Scanner(System.in);
+       
+       Kalkulator kalkulator = new Kalkulator();
+       
+       System.out.println("======Aplikasi Kalkulator Bilangan======");
+       System.out.print("Masukan Angka ke-1 : ");
+       kalkulator.value1 = scanner.nextDouble();
+       System.out.print("Masukan Angka ke-2 : ");
+       kalkulator.value2 = scanner.nextDouble();
+       
+       double tambah = kalkulator.tambahBilangan();
+       double kurang = kalkulator.kurangBilangan();
+       double kali = kalkulator.kaliBilangan();
+       double bagi = kalkulator.bagiBilangan();
+       double sisa = kalkulator.sisaBilangan();
+        
+       System.out.println("Hasil Tambahan : " +tambah);
+       System.out.println("Hasil Pengurangan : "+kurang);
+       System.out.println("Hasil Perkalian : "+kali);
+       System.out.println("Hasil Pembagian : "+bagi);
+       System.out.println("Hasil Sisa : "+sisa);
+    }
+    
+}
